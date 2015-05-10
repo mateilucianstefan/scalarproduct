3ad633f1639577c1d94f81d054a85935caa8dfca
darkluck02 <mateilucianstefan@gmail.com>
2015-05-10 20:37:09 +0300

    initial commit

---
+++ src/main/java/code/jam/App.java
@@ -0,0 +1,13 @@
+package code.jam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
